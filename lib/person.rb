ce2d8c0b2114b64f8db80f875b72cd2981aab577
Nicole Dow <nikki.dow71@gmail.com>
2019-07-31 16:21:41 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
+# class Person
+  
+#   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  
+#   def initialize(name:, birthday:, hair_color:, eye_color:, height:, weight:, handed:, complexion:, t_shirt_size:, wrist_size:, glove_size:, pant_length:, pant_width:)
+#     @name = name
+#     @birthday = birthday
+#     @hair_color = 
+#   end
+  
+# end
+
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  
+def initialize(attributes)
+    attributes.each do |key, val|
+      self.send("#{key}=", val)
+    end
+  end # 
+  
 end
