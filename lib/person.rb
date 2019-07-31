5d07eae5c3b4bbab2337d5dfb2b4d5449135d27e
Nicole Dow <nikki.dow71@gmail.com>
2019-07-31 16:20:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -16,9 +16,9 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
 def initialize(attributes)
-  #   attributes.each do |key, val|
-  #     self.send(("#{key}=", val))
-  #   end
-  # end # 
+    attributes.each do |key, val|
+      self.send(("#{key}=", val))
+    end
+  end # 
   
 end
