8d803642bd2028e32d1983cf019e0a60f40c756f
Nicole Dow <nikki.dow71@gmail.com>
2019-07-31 16:18:40 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -16,7 +16,9 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(attributes)
-    
+    attributes.each do |key, val|
+      binding.pry
+    end
   end
   
 end
